d634fd77b10484e70f1a4de9c3215f0b989b80ce
Abdallah <bahabdoulaye5899@gmail.com>
2021-06-10 08:20:35 +0000

    first commited

---
+++ MadlibsProject/madlibs.py
@@ -0,0 +1,12 @@
+# String concatenation methods
+#youtuber = "Lacharia Media"
+#print("Subscribe to " + youtuber)
+#print("Subscribe to {} ".format(youtuber))
+#print(f"Subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+madlib = f"Programming is so {adj}! It makes me so {verb1} because i love {verb2}. Finally, you fill like {famous_person}."
+print(madlib)
